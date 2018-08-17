46d0aa4a2a074b6f17239637fe633e87b9b7eaee
Kota Nonaka <kota.kaicho@gmail.com>
2018-08-17 22:54:41 +0900

    Modify map of array.

--- src/components/Tags.js
+++ src/components/Tags.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default ({list}) => {
   return list === null ? null : (
     <ul className='tags is-marginless'>
-      {(list || []).map((tag, index) => (
+      {(list).map((tag, index) => (
         <li key={index} className='tag is-rounded is-primary'>{tag}</li>
       ))}
     </ul>
