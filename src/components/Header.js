bb58418f3ed4c4b02b4807ef38f994b3fbd497a3
Kota Nonaka <kota.kaicho@gmail.com>
2018-08-20 18:38:18 +0900

    Fix navbar behavior.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,13 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import Navbar from './Navbar';
 
-const isPathEqualToLocation = (path , location) => (location.pathname === path)
 
 const Header = ({location, title}) => {
   return (
     <header className='hero is-primary'>
       <div className='hero-head'>
-        <nav className="navbar">
-          <div className="container">
-            <div className="navbar-brand">
-              {/* <a className="navbar-item is-size-5 has-text-weight-bold">{title}</a> */}
-              <span className="navbar-burger burger" data-target="navbarMenuHeroA">
-                <span />
-                <span />
-                <span />
-              </span>
-            </div>
-            <div id="navbarMenuHeroA" className="navbar-menu">
-              <div className="navbar-end">
-                <Link to='/' className={'navbar-item' + (isPathEqualToLocation('/', location) ? ' is-active' : '')}>Top</Link>
-                <Link to='/about' className={'navbar-item' + (isPathEqualToLocation('/about', location) ? ' is-active' : '')}>About</Link>
-              </div>
-            </div>
-          </div>
-        </nav>
+        <Navbar location={location}/>
       </div>
       <div className='hero-body has-text-centered'>
         <div className='container'>
---
+++ src/components/Navbar.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const isPathEqualToLocation = (path , location) => (location.pathname === path)
+
+export default class Navbar extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isOpen: false
+    }
+
+    this.toggleOpen = this.toggleOpen.bind(this)
+  }
+
+  toggleOpen() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    })
+  }
+
+  render() {
+    console.log(this.state.isOpen)
+    return (
+      <nav className="navbar">
+        <div className="container">
+          <div className="navbar-brand">
+            {/* <a className="navbar-item is-size-5 has-text-weight-bold">{title}</a> */}
+            <span className={'navbar-burger burger' + (this.state.isOpen ? ' is-active' : '')} data-target="navbarMenuHeroA" onClick={this.toggleOpen}>
+              <span />
+              <span />
+              <span />
+            </span>
+          </div>
+          <div className={'navbar-menu' + (this.state.isOpen ? ' is-active' : '')}>
+            <div className="navbar-end">
+              <Link to='/' className={'navbar-item' + (isPathEqualToLocation('/', this.props.location) ? ' is-active' : '')}>Top</Link>
+              <Link to='/about' className={'navbar-item' + (isPathEqualToLocation('/about', this.props.location) ? ' is-active' : '')}>About</Link>
+            </div>
+          </div>
+        </div>
+      </nav>
+    )
+  }
+}
