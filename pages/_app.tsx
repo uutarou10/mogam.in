3a6ece2bcbae615ee54fd27e1e9280023280a860
Kota Nonaka <kota_nonaka@fringe81.com>
2020-05-20 23:03:12 +0900

    トップ画面のスタイル修正

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,7 +10,9 @@ const App = ({Component, pageProps}: AppProps) => {
       <Head>
         <link href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@300;500;900&display=swap" rel="stylesheet" />
       </Head>
-      <Component {...pageProps} />
+      <div style={{height: '100vh', width: '940px', margin: 'auto'}}>
+        <Component {...pageProps} />
+      </div>
     </>
     );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,39 @@
 import React from 'react';
 import Head from 'next/head'
 import styles from '../styles/home.module.scss';
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className="container">
+    <>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>mogam.in</title>
+        <link rel="icon" href="/favicon.ico"/>
       </Head>
 
-      <main>
-        <header className={styles.header}>
-          <h1 className={styles.header__title}>mogam.in</h1>
-          <div className={styles.header__subTitle}>portfolio ni mierukedo Next.js sawaritakatta dake...</div>
-          <img  className={styles.header__logo} src="/logo.svg" alt="logo" />
-        </header>
-      </main>
-    </div>
-  );
+      <div className={styles.container}>
+        <main>
+
+          <header className={styles.header}>
+            <img className={styles.header__logo} src="/logo.svg" alt="logo"/>
+            <h1 className={styles.header__title}>mogam.in</h1>
+            <div className={styles.header__subTitle}>portfolio ni mierukedo Next.js sawaritakatta dake...</div>
+          </header>
+
+          <nav className={styles.navigation}>
+            <Link href={"/about"}>
+              <a className={styles.navigation__link}>😉 about me</a>
+            </Link>
+            <Link href={"/articles"}>
+              <a className={styles.navigation__link}>📚 articles</a>
+            </Link>
+            <Link href={"/contact"}>
+              <a className={styles.navigation__link}>📱 contact</a>
+            </Link>
+          </nav>
+        </main>
+      </div>
+    </>
+  )
+    ;
 };
