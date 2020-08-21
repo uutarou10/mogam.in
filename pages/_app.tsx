84b62cc94a55e2ea703ea74c00979415812057c3
Kota Nonaka <kota.kaicho@gmail.com>
2020-08-21 22:43:46 +0900

    トップページをなんちゃってレスポンシブ対応

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {AppProps} from "next/app";
 import 'normalize.css/normalize.css';
 import '../styles/index.scss';
+import styles from '../styles/app.module.scss';
 import Head from "next/head";
 
 const App = ({Component, pageProps}: AppProps) => {
@@ -9,8 +10,9 @@ const App = ({Component, pageProps}: AppProps) => {
     <>
       <Head>
         <link href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@300;500;900&display=swap" rel="stylesheet" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
       </Head>
-      <div style={{height: '100vh', width: '940px', margin: 'auto', color: '#E5E5E5'}}>
+      <div className={styles.pageWrap}>
         <Component {...pageProps} />
       </div>
     </>
