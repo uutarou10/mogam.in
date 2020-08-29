f82b4a66154af184a3a5f4dc2bd7c5abcbdc305e
Kota Nonaka <kota.kaicho@gmail.com>
2020-08-30 00:13:13 +0900

    GA用のモジュール作った

---
+++ lib/ga.ts
@@ -0,0 +1,15 @@
+const GA_TRACKING_ID = process.env.GA_TRACKING_ID;
+
+declare global {
+  interface Window {
+    gtag: (a: string, trackingId: string, { page_path: string }) => void;
+  }
+}
+
+export const ga = {
+  pageView: (url: string) => {
+    window.gtag('config', GA_TRACKING_ID, {
+      page_path: url,
+    });
+  },
+};
